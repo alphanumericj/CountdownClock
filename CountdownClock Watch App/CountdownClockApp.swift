d958c5c05034b32234873e6c4e275274ac3d9bbd
Laure Chipman <laure@chipmania.com>
2025-12-05 13:13:49 -0700

    Initial Commit

---
+++ CountdownClock Watch App/CountdownClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountdownClockApp.swift
+//  CountdownClock Watch App
+//
+//  Created by Laure Chipman on 12/5/25.
+//
+
+import SwiftUI
+
+@main
+struct CountdownClock_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CountdownClock/CountdownClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountdownClockApp.swift
+//  CountdownClock
+//
+//  Created by Laure Chipman on 12/5/25.
+//
+
+import SwiftUI
+
+@main
+struct CountdownClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
